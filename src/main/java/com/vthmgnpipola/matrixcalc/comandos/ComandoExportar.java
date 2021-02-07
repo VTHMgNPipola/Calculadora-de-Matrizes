7bd53688fe3f4ff7907fccb09a1c323ec553e16a
VTHMgNPipola <web.anunciante@gmail.com>
2021-02-07 16:07:46 -0300

    Adicionado comando de importação e exportação de comandos.

---
+++ src/main/java/com/vthmgnpipola/matrixcalc/comandos/ComandoExportar.java
@@ -0,0 +1,58 @@
+package com.vthmgnpipola.matrixcalc.comandos;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+@ComandoRegistrado("exportar")
+public class ComandoExportar implements Comando {
+    @Override
+    public boolean checarArgumentos(String args) {
+        return true;
+    }
+
+    @Override
+    public void executar(String args) throws ComandoException {
+        Path path;
+        if (args.isEmpty()) {
+            path = Path.of(System.getProperty("user.home"), "ComandosCM.cms");
+        } else {
+            path = Path.of(args);
+        }
+
+        if (!Files.isRegularFile(path)) {
+            if (!Files.isDirectory(path.getParent())) {
+                throw new ComandoException("Arquivo e diretório parente não existentes!");
+            } else {
+                try {
+                    Files.createFile(path);
+                } catch (IOException e) {
+                    throw new ComandoException("Não foi possível criar o arquivo na pasta especificada!");
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String comando : RegistroComandos.getHistorico()) {
+            sb.append(comando).append("\n");
+        }
+
+        try {
+            Files.writeString(path, sb.toString());
+        } catch (IOException e) {
+            throw new ComandoException("Não foi possível escrever para o arquivo especificado!");
+        }
+
+        System.out.println("Comandos exportados para " + path.toString() + " com sucesso.");
+        System.out.println();
+    }
+
+    @Override
+    public String getDescricao() {
+        return """
+               Exporta todos os comandos no histórico para um arquivo separado. Pode ser útil para debugging, ou simples
+               transferências de cálculos entre computadores diferentes, por exemplo.
+               O local do arquivo deve ser passado como argumento, ou então executar o comando sem argumentos para
+               salvar todos os comandos para o arquivo 'ComandosCM.cms' na pasta do usuário.""";
+    }
+}
---
+++ src/main/java/com/vthmgnpipola/matrixcalc/comandos/ComandoImportar.java
@@ -0,0 +1,63 @@
+package com.vthmgnpipola.matrixcalc.comandos;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+@ComandoRegistrado("importar")
+public class ComandoImportar implements Comando {
+    @Override
+    public boolean checarArgumentos(String args) {
+        return true;
+    }
+
+    @Override
+    public void executar(String args) throws ComandoException {
+        Path path;
+        if (args.isEmpty()) {
+            path = Path.of(System.getProperty("user.home"), "ComandosCM.cms");
+        } else {
+            path = Path.of(args);
+        }
+
+        List<String> comandos;
+        try {
+            comandos = Files.readAllLines(path);
+        } catch (IOException e) {
+            throw new ComandoException("Não foi possível ler o arquivo de comandos!");
+        }
+
+        PrintStream outStream = System.out;
+        System.setOut(new PrintStream(new OutputStream() {
+            @Override
+            public void write(int b) {
+                // Não faz nada, ou seja, desabilita o System.out.
+            }
+        }));
+
+        // Executa todos os comandos
+        for (String comando : comandos) {
+            RegistroComandos.executarSeExistente(comando);
+        }
+
+        // Reabilita o System.out.
+        System.setOut(outStream);
+
+        System.out.println("Arquivo de comandos executado com sucesso!");
+    }
+
+    @Override
+    public String getDescricao() {
+        return """
+               "Importa" comandos de um arquivo, ou seja, executa todos eles, na ordem em que estão.
+               Cada comando no arquivo de comandos deve estar em uma linha separada, e devem ser escritos exatamente
+               como seriam se fossem ser escritos manualmente. É útil para executar os comandos no arquivo gerado pelo
+               comando 'exportar'.
+               Para especificar o arquivo a ser carregado, escreva o nome do arquivo como único argumento a esse
+               comando. Ou então não use nenhum argumento, e o arquivo 'ComandosCM.cms' na pasta do usuário tentará ser
+               carregado.""";
+    }
+}
